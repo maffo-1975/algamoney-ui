c241895d2773e2f8ef8d069cd99241ab0c8bcebd
Marcio Cavalcante de Azevedo <marcioazevedo@caern.local>
2022-03-17 17:07:37 -0300

    10.3. Criando o projeto do curso e instalando o PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TabViewModule } from 'primeng/tabview';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
